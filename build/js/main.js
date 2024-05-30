eb550a99e3d79f375683e315064ecb360ae7ebd8
Anxheloo <anxhelocenollari@gmail.com>
2024-05-30 17:16:18 +0200

    Working with arrays, objects, enums

--- build/js/main.js
+++ build/js/main.js
@@ -1,17 +1,58 @@
 "use strict";
-// BASIC TYPES IN TYPESCRIPT
-let myName = "Anxhelo";
-let meaningOfLife;
-let isLoading;
-let album; //string or number
-myName = "John";
-meaningOfLife = 42;
-isLoading = true;
-album = "Noizy";
-const sum = (a, b) => {
-    return a + b;
+// -------------------------------
+// WORKING WITH ARRAYS IN TS
+// -------------------------------
+let stringArr = ["one", "hey", "Anxhelo"];
+let guitars = ["Strat", "Les Paul", 5150];
+let mixedData = ["EVH", 1985, true];
+// The first array only accepts strings
+stringArr[0] = "John";
+stringArr.push("hey");
+// The second array accepts strings or numbers
+guitars[0] = 1984;
+guitars.unshift("Jim");
+// The third array accepts strings, numbers and booleans
+mixedData = guitars;
+// Any time of data can go into this array cuz we havent specify anything
+let test = [];
+// This is specified to accepts strings
+let bands = [];
+bands.push("halo");
+// In this way we have more control over our datas and order of them.
+let myTuple = ["Dave", 42, true];
+// In this way we specift the types of data our array accepts.
+let myTuple2 = [42, "Dave", true];
+// -------------------------------
+// OBJECTS
+// -------------------------------
+let myObj;
+// Is okej, cuz array is an object type
+myObj = [];
+const exampleObj = {
+    prop1: "Dave",
+    prop2: true,
 };
-let postId;
-let isActive;
-//TypeScript also gives us the correct type when we hover over it.
-let re = /\w+/g;
+let myObject = {
+    name: "Eddie",
+    active: false,
+    albums: [1990, 5150, "OUCABONE"],
+};
+// Here we dont use the second prop of the Guitarist type.
+let myObject2 = {
+    name: "Rimi",
+    albums: [1290, 550, "MIR"],
+};
+const greetGuitarist = (guitarist) => {
+    console.log(`Hello ${guitarist.name}`);
+};
+greetGuitarist(myObject);
+// ENUMS
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,83 @@
-// BASIC TYPES IN TYPESCRIPT
+// -------------------------------
+// WORKING WITH ARRAYS IN TS
+// -------------------------------
+let stringArr = ["one", "hey", "Anxhelo"];
 
-let myName: string = "Anxhelo";
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: string | number; //string or number
+let guitars = ["Strat", "Les Paul", 5150];
 
-myName = "John";
-meaningOfLife = 42;
-isLoading = true;
-album = "Noizy";
+let mixedData = ["EVH", 1985, true];
 
-const sum = (a: number, b: number) => {
-  return a + b;
+// The first array only accepts strings
+stringArr[0] = "John";
+stringArr.push("hey");
+
+// The second array accepts strings or numbers
+guitars[0] = 1984;
+guitars.unshift("Jim");
+
+// The third array accepts strings, numbers and booleans
+mixedData = guitars;
+
+// Any time of data can go into this array cuz we havent specify anything
+let test = [];
+
+// This is specified to accepts strings
+let bands: string[] = [];
+bands.push("halo");
+
+// In this way we have more control over our datas and order of them.
+let myTuple: [string, number, boolean] = ["Dave", 42, true];
+
+// In this way we specift the types of data our array accepts.
+let myTuple2: (string | number | boolean)[] = [42, "Dave", true];
+
+// -------------------------------
+// OBJECTS
+// -------------------------------
+
+let myObj: object;
+
+// Is okej, cuz array is an object type
+myObj = [];
+
+const exampleObj = {
+  prop1: "Dave",
+  prop2: true,
 };
 
-let postId: string | number;
-let isActive: number | boolean;
+// Here we create a type Object and specify its property types. By using "?", we make the property optional
+type Guitarist = {
+  name: string;
+  active?: boolean;
+  albums: (string | number)[];
+};
 
-//TypeScript also gives us the correct type when we hover over it.
-let re: RegExp = /\w+/g;
+let myObject: Guitarist = {
+  name: "Eddie",
+  active: false,
+  albums: [1990, 5150, "OUCABONE"],
+};
+
+// Here we dont use the second prop of the Guitarist type.
+let myObject2: Guitarist = {
+  name: "Rimi",
+  albums: [1290, 550, "MIR"],
+};
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  console.log(`Hello ${guitarist.name}`);
+};
+
+greetGuitarist(myObject);
+
+// ENUMS
+
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}
+
+console.log(Grade.U);
