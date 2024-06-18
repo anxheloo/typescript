0d88164eb516bc4b2fa9ad0fd4badcebb2a2da66
Anxheloo <anxhelocenollari@gmail.com>
2024-06-18 10:24:17 +0200

    asdf

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,13 @@
-// TYPE ALIASES - we can define the variable types and save it in a variable to use as we are doing below.
+class Coder {
+  name: string;
+  music: string;
+  age: number;
+  lang: string;
 
-type stringOrNumber = string | number;
-
-type stringOrNumberArray = (string | number)[];
-
-type Guitarist = {
-  name?: string;
-  active?: boolean;
-  albums: stringOrNumberArray;
-};
-
-// Literal types
-let myName: "Dave";
-
-let username: "Dave" | "John" | "Amy";
-username = "Amy";
-
-//  functions
-const add = (a: number, b: number): number => {
-  return a + b;
-};
-
-// Function that doesnt return anything
-const logMsg = (message: any): void => {
-  console.log(message);
-};
-
-logMsg(add(2, 3));
-
-// We can use type with functions
-type mathFunction = (a: number, b: number) => number;
-const multiply: mathFunction = (a, b) => {
-  return a * b;
-};
-
-logMsg(multiply(3, 3));
-
-// Optional Parameter
-const addAll = (a: number, b: number, c?: number): number => {
-  if (c !== undefined) {
-    return a + b + c;
+  constructor(name: string, music: string, age: number, lang: string) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
   }
-
-  return a + b;
-};
-
-console.log(addAll(3, 3, 3));
-console.log(addAll(3, 3));
-
-// Default param value
-const sumAll = (a: number, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-logMsg(sumAll(2, 2, 2));
-logMsg(sumAll(2, 2));
-
-// Rest Parameters
-const total = (...nums: number[]): number => {
-  return nums.reduce((prev, curr) => prev + curr);
-};
-logMsg(total(1, 2, 3, 4));
+}
